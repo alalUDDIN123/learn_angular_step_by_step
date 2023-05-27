d6cd3c8c05942d2df1dd6e91873e74e865562a9f
alalUDDIN123 <alalu75666@gmail.com>
2023-05-27 20:39:41 +0530

    template_and_data_binding_in_angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align: center;">
+      <h1>Hello Angular</h1>
+      <p>{{message}}</p>
+      <!-- The message property is a data binding expression enclosed in curly braces -->
+      <button (click)="updateMessage()">Update message</button>
+      <!-- The button triggers the updateMessage() method when clicked -->
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'learn_angular';
+  message = 'Initial message';
+  // The initial value of the message property
+
+  updateMessageClass = 'Message updated successfully';
+  // The value to be assigned to the message property when updated
+
+  updateMessage() {
+    this.message = this.updateMessageClass;
+    // Updates the message property with the value in updateMessageClass
+  }
 }
+
