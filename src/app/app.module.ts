09421a3f0db22b884606f04a155e2cb7fab6aaa6
alalUDDIN123 <alalu75666@gmail.com>
2023-06-04 10:55:42 +0530

    basic_routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+ 
   ],
   imports: [
     BrowserModule,
